fe7547832d91fc32752d1262ec18912fd3f9b3f8
vaibhav <borkarv908@gmail.com>
2025-11-15 15:10:23 +0530

    test your initial api woeking or not

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
+//test the initial api 
+// app.get("/home", (req, res)=>{
+//   return res.status(200).json({
+//     message: "i am comming from backend",
+//     success: true
+//   })
+// });
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +25,5 @@ app.use(cors(corsOption));
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`server is running on server https://localhost${PORT}`);
+  console.log(`server is running on server https://localhost${PORT}`);//create server 
 });
